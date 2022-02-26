fa0d09c05cc5b46ed56fc8b4f7ce8ed656ba933d
bamkhatri <bamkhatri24@gmail.com>
2022-02-26 22:09:12 +0545

    feed option and share completed

--- frontend/src/Components/Feed/Feed.js
+++ frontend/src/Components/Feed/Feed.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Share from '../Share/Share'
 import './Feed.css'
 const Feed = () => {
-  return <div className='feed'>Feed</div>
+  return (
+    <div className='feed'>
+      <div className='feedWrapper'>
+        <Share />
+      </div>
+    </div>
+  )
 }
 
 export default Feed
