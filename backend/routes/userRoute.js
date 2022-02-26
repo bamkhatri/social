158d2f2741964cb71af4f98810b94f5b52427c87
bamkhatri <bamkhatri24@gmail.com>
2022-02-26 12:42:53 +0545

    user routes are completed and post model is create

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -66,7 +66,7 @@ router.put('/:id/follow', async (req, res) => {
         })
         await currentUser.updateOne({
           $push: {
-            following: req.body.userId,
+            following: req.params.id,
           },
         })
         res.status(200).json('User has been followed')
@@ -80,6 +80,34 @@ router.put('/:id/follow', async (req, res) => {
     return res.status(403).json('You cannot follow yourself')
   }
 })
+
 //unfollow user
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId)
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: {
+            followers: req.body.userId,
+          },
+        })
+        await currentUser.updateOne({
+          $pull: {
+            following: req.params.id,
+          },
+        })
+        res.status(200).json('User has been unfollow')
+      } else {
+        res.status(403).json('You  dont  follow this user')
+      }
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+  } else {
+    return res.status(403).json('You cannot unfollow yourself')
+  }
+})
 
 module.exports = router
