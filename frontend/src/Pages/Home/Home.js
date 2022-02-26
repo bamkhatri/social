15847b1a36411b4beb2c13024ed8cad7181710c0
bamkhatri <bamkhatri24@gmail.com>
2022-02-26 19:00:32 +0545

    navbar completed

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import Feed from '../../Components/Feed/Feed'
+import Leftbar from '../../Components/Leftbar/Leftbar'
+import Rightbar from '../../Components/Rightbar/Rightbar'
+import Toolbar from '../../Components/Toolbar/Toolbar'
+import './Home.css'
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <Toolbar />
+      <div className='homeContainer'>
+        <Leftbar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  )
 }
 
 export default Home
