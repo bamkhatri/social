69ea7e391b8b59a47b156a17c7f8f1ff3662e0a0
bamkhatri <bamkhatri24@gmail.com>
2022-02-26 23:12:52 +0545

    right bar complete

--- frontend/src/Components/Rightbar/Rightbar.js
+++ frontend/src/Components/Rightbar/Rightbar.js
@@ -1,7 +1,166 @@
 import React from 'react'
 import './Rightbar.css'
 const Rightbar = () => {
-  return <div className='rightbar'>Rightbar</div>
+  return (
+    <div className='rightbar'>
+      <div className='rightbarWrapper'>
+        <div className='birthdayContainer'>
+          <img src='assets/gift.png' alt='' className='birthdayImg' />
+          <span className='birthdayText'>
+            <b>Abhishek Joshi </b>and <b>2 other friends</b> have a birthday
+            today.
+          </span>
+        </div>
+        <img src='assets/ad.png' alt='' className='rightbarAd' />
+        <h4 className='rightbarTitle'>Online Friends</h4>
+        <ul className='friendList'>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>{' '}
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='assets/person/3.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Rightbar
